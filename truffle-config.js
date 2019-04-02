935ad0776545fc8db190ddc98091e9328e0aa702
Andrew B Coathup <andrew@coathup.net>
2019-04-02 12:25:43 +1100

    Testnet deploy

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "${process.env.INFURA_API_KEY}";
 
 module.exports = {
   /**
@@ -68,6 +67,34 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, `https://rinkeby.infura.io/${infuraKey}`),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, `https://ropsten.infura.io/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.MAINNET_MNEMONIC, `https://mainnet.infura.io/v3/${infuraKey}`),
+      network_id: 1,       // 
+      gas: 5500000,  
+      gasPrice: 7500000000,  // check https://ethgasstation.info/
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {

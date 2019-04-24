42feebd7d1597a5a5eb07458bc15d0b5fa3a2153
Bevan <bevan.barton@gmail.com>
2019-04-24 11:16:45 -0700

    Update PeepethBadges.sol
    Removing comments

--- contracts/PeepethBadges.sol
+++ contracts/PeepethBadges.sol
@@ -13,7 +13,7 @@ import "openzeppelin-solidity/contracts/introspection/ERC165.sol";
  * Moreover, it includes approve all functionality using operator terminology
  * @dev see https://github.c/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
-contract PeepethBadges is ERC165, ERC721, ERC721Enumerable, IERC721Metadata, MinterRole {
+contract PeepethBadges is ERC165, ERC721, ERC721Enumerable, IERC721Metadata, MinterRole, Ownable {
 
   // Mapping from token ID to badge
   mapping (uint256 => uint256) private _tokenBadges;
@@ -23,6 +23,9 @@ contract PeepethBadges is ERC165, ERC721, ERC721Enumerable, IERC721Metadata, Min
 
   // Token symbol
   string private _symbol;
+
+  // Base URI for badge data
+  string private _baseTokenURI;
 
   bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
   /*
@@ -38,6 +41,7 @@ contract PeepethBadges is ERC165, ERC721, ERC721Enumerable, IERC721Metadata, Min
   constructor () public {
     _name = "Peepeth Badges";
     _symbol = "PB";
+    _baseTokenURI = "https://peepeth.com/b/";
 
     // register the supported interfaces to conform to ERC721 via ERC165
     _registerInterface(_INTERFACE_ID_ERC721_METADATA);
@@ -76,8 +80,16 @@ contract PeepethBadges is ERC165, ERC721, ERC721Enumerable, IERC721Metadata, Min
    * @dev Gets the base token URI
    * @return string representing the base token URI
    */
-  function baseTokenURI() public pure returns (string memory) {
-    return "https://peepeth.com/badges/api/badge/";
+  function baseTokenURI() public view returns (string memory) {
+    return _baseTokenURI;
+  }
+
+
+  /**
+   * @dev Set the base token URI
+   */
+  function setBaseTokenURI(string memory baseURI) public onlyOwner {
+    _baseTokenURI = baseURI;
   }
 
   /**
@@ -120,4 +132,21 @@ contract PeepethBadges is ERC165, ERC721, ERC721Enumerable, IERC721Metadata, Min
   function _getNextTokenId() private view returns (uint256) {
     return totalSupply().add(1);
   }
+
+
+
+  /**
+   * @dev Only owner can addMinter
+   */
+  function addMinter(address account) public onlyOwner {
+    _addMinter(account);
+  }
+
+  /**
+   * @dev Only owner can renounce specific minters
+   */
+  function renounceMinter(address account) public onlyOwner {
+    _removeMinter(msg.sender);
+  }
+
 }

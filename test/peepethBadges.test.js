ff6f7f9a43361fa5b3a85df85a904a34216dc3c7
Andrew B Coathup <andrew@coathup.net>
2019-04-29 11:34:35 +1000

    Add tests

--- test/peepethBadges.test.js
+++ test/peepethBadges.test.js
@@ -1,12 +1,11 @@
 const { BN, constants, expectEvent, shouldFail } = require('openzeppelin-test-helpers');
 const PeepethBadges = artifacts.require('PeepethBadges')
 
-contract('PeepethBadges', function([minter, anotherAccount]) {
-  let horses
+contract('PeepethBadges', function([minter, anotherAccount, thirdAccount]) {
   const name = "Peepeth Badges";
   const symbol = "PB";
-  const baseTokenUri = "https://peepeth.com/badges/api/badge/";
-  const badgeCount = 16
+  const baseTokenURI = "https://peepeth.com/b/";
+  const badgeCount = 8
 
   beforeEach(async function() {
     peepethBadges = await PeepethBadges.new({ from: minter })
@@ -20,6 +19,8 @@ contract('PeepethBadges', function([minter, anotherAccount]) {
       assert.equal(symbol, _symbol);
       const _totalSupply = await peepethBadges.totalSupply();
       assert.equal(0, _totalSupply);
+      const _baseTokenURI = await peepethBadges.baseTokenURI();
+      assert.equal(baseTokenURI, _baseTokenURI);
     })
   });
 
@@ -34,8 +35,8 @@ contract('PeepethBadges', function([minter, anotherAccount]) {
       assert.equal(1, anotherAccountBalance);
       const _owner = await peepethBadges.ownerOf(new BN(1));
       assert.equal(anotherAccount, _owner);
-      const _tokenUri = await peepethBadges.tokenURI(new BN(1));
-      assert.equal(`${baseTokenUri}1`, _tokenUri);
+      const _tokenURI = await peepethBadges.tokenURI(new BN(1));
+      assert.equal(`${baseTokenURI}1`, _tokenURI);
       const _tokenbadge = await peepethBadges.tokenBadge(new BN(1));
       _tokenbadge.should.be.bignumber.equal(new BN(0));
     });
@@ -53,22 +54,73 @@ contract('PeepethBadges', function([minter, anotherAccount]) {
       assert.equal(badgeCount, anotherAccountBalance);
 
       var _owner;
-      var _tokenUri;
+      var _tokenURI;
       var _tokenbadge;
       var _tokenId;
       for (var i=0; i < badgeCount; i++) {
         _tokenId = new BN(i + 1);
         _owner = await peepethBadges.ownerOf(_tokenId);
         assert.equal(anotherAccount, _owner);
-        _tokenUri = await peepethBadges.tokenURI(_tokenId);
-        assert.equal(`${baseTokenUri}${i + 1}`, _tokenUri);
+        _tokenURI = await peepethBadges.tokenURI(_tokenId);
+        assert.equal(`${baseTokenURI}${i + 1}`, _tokenURI);
         _tokenbadge = await peepethBadges.tokenBadge(_tokenId);
         _tokenbadge.should.be.bignumber.equal(new BN(i + 1));
       }
     });
-    
+
+    it('change baseTokenURI', async function() {
+      const _baseTokenURI = await peepethBadges.baseTokenURI();
+      assert.equal(baseTokenURI, _baseTokenURI);
+      const newBaseTokenURI = "https://peepeth.com/b2/";
+      peepethBadges.setBaseTokenURI(newBaseTokenURI);
+      const _newBaseTokenURI = await peepethBadges.baseTokenURI();
+      assert.equal(newBaseTokenURI, _newBaseTokenURI);
+    })
+
+    it('add minter', async function() {
+      var isMinter = await peepethBadges.isMinter(anotherAccount);
+      assert.equal(false, isMinter);
+      await peepethBadges.addMinter(anotherAccount);
+      isMinter = await peepethBadges.isMinter(anotherAccount);
+      assert.equal(true, isMinter);
+
+      await peepethBadges.mint(anotherAccount, new BN(0), { from: anotherAccount });
+      const _totalSupply = await peepethBadges.totalSupply();
+      assert.equal(1, _totalSupply);      
+    })
+
+    it('account recounce minter', async function() {
+      await peepethBadges.addMinter(anotherAccount);
+      await peepethBadges.methods['renounceMinter()']({ from: anotherAccount });
+
+      var isMinter = await peepethBadges.isMinter(anotherAccount);
+      assert.equal(false, isMinter);
+    })
+
+    it.skip('owner recounce minter', async function() {
+      await peepethBadges.addMinter(anotherAccount);
+      await peepethBadges.methods['recounceMinter(address)'](anotherAccount);
+
+      var isMinter = await peepethBadges.isMinter(anotherAccount);
+      assert.equal(false, isMinter);
+    })
+
     it('non-minter cannot mint a token', async function() {
       await shouldFail.reverting(peepethBadges.mint(anotherAccount, new BN(0), { from: anotherAccount }));
+    })
+    
+    it('non-owner cannot change baseTokenURI', async function() {
+      const newBaseTokenURI = "https://peepeth.com/b2/";
+      await shouldFail.reverting(peepethBadges.setBaseTokenURI(newBaseTokenURI, { from: anotherAccount }));
+      const _baseTokenURI = await peepethBadges.baseTokenURI();
+      assert.equal(baseTokenURI, _baseTokenURI);
+    })
+    
+    it('non-owner minter cannot add minter', async function() {
+      await peepethBadges.addMinter(anotherAccount);
+      await shouldFail.reverting(peepethBadges.addMinter(thirdAccount, { from: anotherAccount }));
+      var isMinter = await peepethBadges.isMinter(thirdAccount);
+      assert.equal(false, isMinter);
     })
   });
 })
